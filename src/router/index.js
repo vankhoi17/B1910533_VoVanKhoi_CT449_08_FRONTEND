a2c4e77f01fb37273ce04d4f1f919d9a70e8106e
quynh <quynhb191058@student.ctu.edu.vn>
2023-03-22 10:34:46 +0700

    Cai dat trang loi 404

--- src/router/index.js
+++ src/router/index.js
@@ -6,6 +6,11 @@ path: "/",
 name: "contactbook",
 component: ContactBook,
 },
+{
+    path: "/:pathMatch(.*)*",
+    name: "notfound",
+    component: () => import("@/views/NotFound.vue"),
+    },
 ];
 const router = createRouter({
 history: createWebHistory(import.meta.env.BASE_URL),
