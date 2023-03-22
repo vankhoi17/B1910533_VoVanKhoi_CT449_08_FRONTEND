e8ea13f3371312bbc3b4e4fe57c1fea27c3700cd
quynh <quynhb191058@student.ctu.edu.vn>
2023-03-22 10:30:44 +0700

    Hien thi danh sach cac contact lay ve tu server

--- vite.config.js
+++ vite.config.js
@@ -8,5 +8,11 @@ export default defineConfig({
   plugins: [vue()],
   server: {
     port: 3001,
-    },    
-});
+    proxy: {
+    "/api": {
+    target: "http://localhost:3000/",
+    changeOrigin: true,
+    },
+    }
+    },
+    });    
